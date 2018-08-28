ca821e2039456c75b24441122a37360de17bb5b3
Robert Pense <robert.pense@diva-e.com>
2018-08-28 13:02:23 +0200

    Add Katas

--- src/main/kotlin/difficulty3/Difficulty3Katas.kt
+++ src/main/kotlin/difficulty3/Difficulty3Katas.kt
@@ -44,5 +44,53 @@ class Difficulty3Katas {
         fun bouncingBall(h:Double, bounce:Double, window:Double) : Int {
             return 0
         }
+
+        /**
+         * Two tortoises named A and B must run a race. A starts with an average speed of 720 feet per hour. Young B knows she runs faster than A, and furthermore has not finished her cabbage.
+         *
+         * When she starts, at last, she can see that A has a 70 feet lead but B's speed is 850 feet per hour. How long will it take B to catch A?
+         *
+         * More generally: given two speeds v1 (A's speed, integer > 0) and v2 (B's speed, integer > 0) and a lead g (integer > 0) how long will it take B to catch A?
+         *
+         * The result will be an array [hour, min, sec] which is the time needed in hours, minutes and seconds (round down to the nearest second) or a string in some languages.
+         *
+         * If v1 >= v2 then return nil, nothing, null, None or {-1, -1, -1} for C++, C, Go, Nim, [] for Kotlin or "-1 -1 -1".
+         *
+         * Examples:
+         *
+         * race(720, 850, 70) => [0, 32, 18] or "0 32 18"
+         * race(80, 91, 37)   => [3, 21, 49] or "3 21 49"
+         */
+        fun race(v1:Int, v2:Int, g:Int) : IntArray {
+            return intArrayOf(0)
+        }
+
+        /**
+         * We are still with squared integers.
+         * Given 4 integers a, b, c, d we form the sum of the squares of a and b and then the sum of the squares of c and d. We multiply the two sums hence a number n and we try to decompose n in a sum of two squares e and f (e and f integers >= 0) so that n = e² + f².
+         *
+         * More: e and f must result only from sums (or differences) of products between on the one hand (a, b) and on the other (c, d) each of a, b, c, d taken only once.
+         *
+         * Suppose we have a = 1, b = 2, c = 1, d = 3. First we calculate the sums 1² + 2² = 5 and 1² + 3² = 10 hence n = 50.
+         *
+         * 50 = 1² + 7² or 50 = 7² + 1² (we'll consider that these two solutions are the same) or 50 = 5² + 5².
+         *
+         * The return of our function will be an array of subarrays (in C an array of Pairs) sorted on the first elements of the subarrays. In each subarray the lower element should be the first.
+         *
+         * prod2sum(1, 2, 1, 3) should return [[1, 7], [5, 5]]
+         * prod2sum(2, 3, 4, 5) should return [[2, 23], [7, 22]]
+         *
+         * because (2² + 3²) * (4² + 5²) = 533 = (7² + 22²) = (23² + 2²)
+         *
+         * prod2sum(1, 2, 2, 3) should return [[1, 8], [4, 7]]
+         *
+         * prod2sum(1, 1, 3, 5) should return [[2, 8]] (there are not always 2 solutions).
+         *
+         * Hint Take a sheet of paper and with a bit of algebra try to write the product of squared numbers in another way.
+         */
+        fun prod2Sum(a:Long, b:Long, c:Long, d:Long):List<LongArray> {
+            return listOf(longArrayOf(0))
+        }
+
     }
 }
